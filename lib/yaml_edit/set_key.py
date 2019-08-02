09fadf291eff21e5121eb65e8d308f4ce3e18537
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-02 15:28:45 -0500

    fix indentation after set_key

--- lib/yaml_edit/set_key.py
+++ lib/yaml_edit/set_key.py
@@ -1,4 +1,5 @@
 import ruamel.yaml
+import regex as re
 import json
 import sys
 
@@ -18,6 +19,12 @@ def set_by_path(data, path, value):
     """Set a value in a nested object in data by item sequence."""
     get_by_path(data, path[:-1])[path[-1]] = value
 
+# detect the indent
+indent_match = re.search(r'^ +', yaml_string, flags=re.MULTILINE)
+if indent_match == None:
+    indent = 4
+else:
+    indent = len(indent_match[0])
 
 # parse the yaml
 data = ruamel.yaml.round_trip_load(yaml_string)
@@ -28,4 +35,4 @@ new_value = json.loads(new_key_value)
 # set the value
 set_by_path(data, path_to_element, new_value)
 # output the data
-print(ruamel.yaml.round_trip_dump(data))
+print(ruamel.yaml.round_trip_dump(data, indent=indent, block_seq_indent=indent))
