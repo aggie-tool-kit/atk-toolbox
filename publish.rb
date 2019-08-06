9b242838f71e57046746d8de720060c45c6d6038
Jeff Hykin <jeffhykin@jeffs-imac.lan>
2019-08-06 14:52:41 -0500

    _

--- publish.rb
+++
@@ -1,5 +0,0 @@
-require 'atk_toolbox'
--"gem bump" or exit
--"gem build atk_toolbox.gemspec" or exit
-require_relative './lib/atk_toolbox/version.rb'
--"gem push \"atk_toolbox-#{AtkToolbox::VERSION}.gem\"" or exit
