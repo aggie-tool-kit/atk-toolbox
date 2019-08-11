66a965e107ecdc650925bd9dfa609a3b79d65ea7
Jeff Hykin <jeffhykin@Jeffs-iMac.local>
2019-08-11 09:10:32 -0500

    remove duplicate require indent

--- lib/atk/extra_file_utils.rb
+++ lib/atk/extra_file_utils.rb
@@ -3,6 +3,7 @@ require 'fileutils'
 require 'pathname'
 require 'open-uri'
 require_relative './os'
+require_relative './remove_indent'
 
 if OS.is?("unix")
     HOME = Etc.getpwuid.dir
@@ -19,17 +20,6 @@ class String
         else
             File.join(self, next_string)
         end
-    end
-    
-    # this is for having docstrings that get their indent removed
-    # this is used frequently for multi-line strings and error messages
-    # example usage 
-    # puts <<-HEREDOC.remove_indent
-    # This command does such and such.
-    #     this part is extra indented
-    # HEREDOC
-    def remove_indent
-        gsub(/^[ \t]{#{self.match(/^[ \t]*/)[0].length}}/, '')
     end
 end
 
--- lib/atk/remove_indent.rb
+++ lib/atk/remove_indent.rb
@@ -1,9 +1,11 @@
 class String
+    # this is for having docstrings that get their indent removed
+    # this is used frequently for multi-line strings and error messages
     # example usage 
-        # puts <<-HEREDOC.remove_indent
-        #     This is a string
-        #     this part is extra indented
-        # HEREDOC
+    # puts <<-HEREDOC.remove_indent
+    # This command does such and such.
+    #     this part is extra indented
+    # HEREDOC
     def remove_indent
         gsub(/^[ \t]{#{self.match(/^[ \t]*/)[0].length}}/, '')
     end
