e79d0294d089862e2fb01972f9fa7644b00b743a
Jeff Hykin <jeff.hykin@gmail.com>
2019-12-22 12:58:35 -0600

    add the install command

--- lib/atk/atk_info.rb
+++ lib/atk/atk_info.rb
@@ -136,6 +136,53 @@ module Atk
     def self.not_yet_implemented()
         puts "Sorry, this feature is still under development"
     end
+    
+    def self.install(*args)
+        if args.size == 0
+            # 
+            # create the file structure if it doesnt exist
+            # 
+            FS.makedirs(HOME/"atk"/"installers")
+            # download the files
+            if not FS.exist?(HOME/"atk"/"core.yaml")
+                FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/core.yaml'       , to: HOME/"atk"/"core.yaml")
+            end
+            if not FS.exist?(HOME/"atk"/"installers.yaml")
+                FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/installers.yaml' , to: HOME/"atk"/"installers.yaml")
+            end
+
+            #
+            # overwrite the commands
+            # 
+
+            # atk
+            atk_command_download_path = Atk.temp_path("atk.rb")
+            FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/atk'     , to: atk_command_download_path)
+            Console.set_command("atk", FS.read(atk_command_download_path))
+
+            # project
+            project_command_download_path = Atk.temp_path("project.rb")
+            FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/project' , to: project_command_download_path)
+            Console.set_command("project", FS.read(project_command_download_path))
+
+            # the project run alias
+            local_command_download_path = Atk.temp_path("local_command.rb")
+            FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/_'      , to: local_command_download_path)
+            Console.set_command("_", FS.read(local_command_download_path))
+
+            # 
+            # print success
+            # 
+            puts ""
+            puts ""
+            puts ""
+            puts "=============================="
+            puts "        ATK installed "
+            puts "=============================="
+        else
+            Atk.not_yet_implemented()
+        end
+    end
 end
 
 
