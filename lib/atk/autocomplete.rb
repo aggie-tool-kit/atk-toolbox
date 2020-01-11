3a8b3b71758213599b2787b0efe8b3f9fafd6f7d
Jeff Hykin <jeff.hykin@gmail.com>
2020-01-11 13:36:10 -0600

    version bump

--- lib/atk/autocomplete.rb
+++ lib/atk/autocomplete.rb
@@ -1,12 +1,13 @@
 module Atk
     def self.autocomplete(which_command)
-        if which_command == '_'
-            require_relative './info.rb'
-            begin
-                puts Info.commands().keys.map { |each| each.gsub(' ', '\ ') }.join(' ')
-            rescue => exception
-                puts ""
-            end
-        end
+        # FUTURE: correct the bugs
+        # if which_command == '_'
+        #     require_relative './info.rb'
+        #     begin
+        #         puts Info.commands().keys.map { |each| each.gsub(' ', '\ ') }.join(' ')
+        #     rescue => exception
+        #         puts ""
+        #     end
+        # end
     end
 end
